62f6146b708269ab2af80dbdd966a98ffb148745
azatcelik13 <azatcelik010196@gmail.com>
2022-02-18 23:12:45 +0300

    testdeneme

---
+++ ucgen/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        int a,b ;
+
+        double c ;
+        Scanner kenar = new Scanner(System.in);
+
+        System.out.print("a kenarını giriniz=");
+        a = kenar.nextInt();
+        System.out.print("b kenarını giriniz=");
+        b = kenar.nextInt();
+        c = Math.sqrt((a*a)+(b*b));
+
+
+
+        System.out.println("Alan="+(a*b)/2);
+
+        System.out.println("Hipotenüs="+c);
+        System.out.println("çevre="+(a+b+c));
+
+
+
+    }
+}
