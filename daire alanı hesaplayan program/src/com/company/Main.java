486af40d2c6103ededf5c9ff8e5ffa0baabf054d
azatcelik13 <azatcelik010196@gmail.com>
2022-02-19 23:27:39 +0300

    lllll

--- daire alanı hesaplayan program/src/com/company/Main.java
+++ daire alanı hesaplayan program/src/com/company/Main.java
@@ -7,12 +7,16 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner kubi= new Scanner(System.in);
-        double r , pi=3.14;
+        double r , pi=3.14 ,a;
         System.out.print("Yarıçapı giriniz=");
         r = kubi.nextDouble();
+        System.out.print("merkez açısınını giriniz=");
+        a = kubi.nextDouble();
         double alan =r*r*pi;
         double cevre=2*pi*r;
+        double daire=(pi*(r*r)*a) / 360;
         System.out.println("Dairenin alanı="+alan);
         System.out.println("Dairenin çevresi="+cevre);
+        System.out.println("daire dilimi alanı="+daire);
     }
 }
